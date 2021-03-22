82a2ab18b044dadcf94b60bf2b8b621ca5a7a198
marvin marjan <80416322+MarvinChaves@users.noreply.github.com>
2021-03-22 12:51:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # creation day = 13/03/2021
-# last update = 17/03/2021
-# version = 0.0.3
-# goal = 500 lines
-# lines = 397
+# last update = 19/03/2021
+# version = 0.1.0
+# goal = 700 lines
+# lines = 581
 
 '''
 
@@ -36,7 +36,22 @@ update log {
 
     17/03/2021 {
 
-        pass
+       1 - userLog function added
+
+    }
+
+    18/03/2021 {
+
+        1 - 1 new function
+
+    }
+
+    19/03/2021 {
+
+        1 - bug fix
+        2 - group function added
+        3 - varsList added
+        4 - if function added
 
     }
 
@@ -122,9 +137,11 @@ def log():
     isBig = False
     varLog = False
     isMath = False
+    cont = False
     isMathing = False
     isVarMath = False
     listLen = len(functionName)
+
     if listLen > 2:
         isBig = True
     
@@ -141,10 +158,12 @@ def log():
     try:
         if type(int(functionName[1])) == int:
             isMathing = True
+            cont = False
 
     except Exception as erro:
         if type(str(functionName[1])) == str:
             isMathing = False
+            cont = True
 
     else:
         if isMathing and len(functionName) >= 3:
@@ -179,12 +198,15 @@ def log():
                     sleep(1)
                     print(int(functionName[1]) / int(functionName[3]))
                     isMath = True
+        
+    if cont:
+        if functionName[2] == '+':
+            sleep(1)
+            print('==' * 50)
+            sleep(1)
+            print(str(functionName[1]) + str(functionName[3]))
+            isMath = True
     
-    t = 0
-    l = 0
-    ready = False
-                
-   
     if isBig and not isVarMath and not isMath:
         length = len(functionName)
         i = 0
@@ -264,16 +286,14 @@ def var():
         elif type(str(functionName[3])) == str:
             strValue = True
         
-        elif type(int(functionName[3].capitalize())) == int:
-            print('é')
+        elif type(int(functionName[3])) == int:
+            intValue = True
 
         elif type(float(functionNam[3])) == float:
             floatValue = True
         
         elif type(bool(functionName[3])) == bool:
             boolValue = True
-
-        print(intValue, strValue, floatValue, boolValue)
 
         if strValue:
             varValue = str(functionName[3])
@@ -312,8 +332,6 @@ def var():
     else:
         varList.append([varName, [varValue]])
         isBigVar = False
-
-    print(varList)
 
 
 # ============================================== userLog def =======================================
@@ -354,7 +372,279 @@ def varsList():
     sleep(1)
     print('==' * 50)
     sleep(1)
-    print(varList)
+    o = 0
+    for string in varList:
+        print(f"\n{varList[o][0]} = {''.join(varList[o][1])}")
+        o += 1
+
+
+
+def group():
+    got = False
+    if functionName[1] == 'var':
+        o = 0
+        while True:
+            if functionName[2] in varList[o]:
+                got = True
+            
+            else:
+                o += 1
+                
+            if got:
+                sleep(1)
+                print('==' * 50)
+                sleep(1)
+                groupList = []
+                for string in varList[o][1]:
+                    for value in string:
+                        groupList.append(value)
+
+
+                try:
+                    value = value.split()
+                    varList[o][1] = int(value)
+                    if type(varList[o][1]) == int:
+                        print(f'Type of {functionName[2]}: \n\n {cyan} <int>{white}')
+                        break
+
+                except:
+                    try:
+                        varList[o][1] = float(value)
+                        if type(varList[o][1]) == float:
+                            print(f'Type of {functionName[2]}: \n\n {cyan} <float>{white}')
+                            break
+                
+                    
+                    except:
+                        try:
+                            word = ''.join(groupList)
+                            if word == 'True' or word == 'False':
+                                print(f'Type of {functionName[2]}: \n\n {cyan} <bool>{white}')
+                                break
+            
+                        
+                        except:
+                            try:
+                                if type(varList[o][1]) == list:
+                                    print(f'Type of {functionName[2]}: \n\n {cyan} <list>{white}')
+                                    break
+                        
+
+                            except:
+                                try:
+                                    varList[o][1] = str(value)
+                                    if type(varList[o][1]) == str:
+                                        print(f'Type of {functionName[2]}: \n\n {cyan} <str>{white}')
+                                        break
+                                
+                                except:
+                                    pass
+
+                                else:
+                                    pass
+
+                            else:
+                                pass
+                        else:
+                            pass
+                    
+                    else:
+                        pass
+                
+                else:
+                    pass
+                    
+
+# ============================================== If def ==============================================
+
+def If():
+
+    def main():
+        o = 0
+        p = 0
+        breaker = False
+        
+        for string in varList[o][0]:
+            
+            while True:
+                if breaker:
+                    break
+                else:
+                    pass
+
+                if functionName[1] in varList[o][0]:
+                    for item in varList[p][0]:
+                        while True:
+                            if breaker:
+                                break
+                            else:
+                                pass
+
+                            if functionName[3] in varList[p][0]:
+                                if functionName[4] == 'log':
+                                    if functionName[5] == 'var':
+                                        o = 0
+                                        for value in varList:
+                                            if breaker:
+                                                break
+                                            else:
+                                                pass
+
+                                            while True:
+                                                if breaker:
+                                                    break
+                                                else:
+                                                    pass
+
+                                                if functionName[6] in varList[o]:
+                                                    if functionName[2] == '==':
+                                                        if varList[o][1] == varList[p][1]:
+                                                            sleep(1)
+                                                            print('==' * 50)
+                                                            sleep(1)
+                                                            print(' '.join(varList[o][1]))
+                                                            breaker = True
+                                                            break
+                                                    
+                                                    elif functionName[2] == '!=':
+                                                        if varList[o][1] != varList[p][1]:
+                                                            sleep(1)
+                                                            print('==' * 50)
+                                                            sleep(1)
+                                                            print(' '.join(varList[o][1]))
+                                                            breaker = True
+                                                            break
+                                                    
+                                                    elif functionName[2] == '>':
+                                                        if varList[o][1] > varList[p][1]:
+                                                            sleep(1)
+                                                            print('==' * 50)
+                                                            sleep(1)
+                                                            print(' '.join(varList[o][1]))
+                                                            breaker = True
+                                                            break
+                                                    
+                                                    elif functionName[2] == '<':
+                                                        if varList[o][1] < varList[p][1]:
+                                                            sleep(1)
+                                                            print('==' * 50)
+                                                            sleep(1)
+                                                            print(' '.join(varList[o][1]))
+                                                            breaker = True
+                                                            break
+                                                    
+                                                    elif functionName[2] == '>=':
+                                                        if varList[o][1] >= varList[p][1]:
+                                                            sleep(1)
+                                                            print('==' * 50)
+                                                            sleep(1)
+                                                            print(' '.join(varList[o][1]))
+                                                            breaker = True
+                                                            break
+                                                    
+                                                    elif functionName[2] == '<=':
+                                                        if varList[o][1] <= varList[p][1]:
+                                                            sleep(1)
+                                                            print('==' * 50)
+                                                            sleep(1)
+                                                            print(' '.join(varList[o][1]))
+                                                            breaker = True
+                                                            break
+                                                    
+                                                    else:
+                                                        pass
+
+                                                else:
+                                                    o += 1
+
+                                    elif functionName[5] != 'var':
+                                        try:
+                                            if type(int(functionName[5])) == int:
+                                                if functionName[6] == '+':
+                                                    sleep(1)
+                                                    print('==' * 50)
+                                                    sleep(1)
+                                                    print(int(functionName[5]) + int(functionName[7]))
+                                                    breaker = True
+                                                    break
+
+                                                elif functionName[6] == '-':
+                                                    sleep(1)
+                                                    print('==' * 50)
+                                                    sleep(1)
+                                                    print(int(functionName[5]) - int(functionName[7]))
+                                                    breaker = True
+                                                    break
+                                                    
+                                                elif functionName[6] == '*':
+                                                    sleep(1)
+                                                    print('==' * 50)
+                                                    sleep(1)
+                                                    print(int(functionName[5]) * int(functionName[7]))
+                                                    breaker = True
+                                                    break
+
+                                                elif functionName[6] == '/':
+                                                    sleep(1)
+                                                    print('==' * 50)
+                                                    sleep(1)
+                                                    print(int(functionName[5]) / int(functionName[7]))
+                                                    breaker = True
+                                                    break
+                                        except:
+                                            if type(str(functionName[5])) == str:
+                                                if len(functionName) == 6:
+                                                    sleep(1)
+                                                    print('==' * 50)
+                                                    sleep(1)
+                                                    print(str(functionName[5]))
+                                                    breaker = True
+                                                    break
+                                                
+                                                elif len(functionName) > 6 :
+                                                    sleep(1)
+                                                    print('==' * 50)
+                                                    sleep(1)
+                                                    leng = len(functionName)
+                                                    m = 5
+                                                    for value in range(leng - 5):
+                                                        print(functionName[m], end=' ')
+                                                        m += 1
+                                                        if m == leng:
+                                                            breaker = True
+                                                            break
+                                
+                                elif functioName[4] == 'var':
+                                    
+                                    if functionName[6] == '=':
+                                        varName = functionName[5]
+                                        varValue = functionName[7]
+                                                varList.append([varName, [varValue]])
+                                                print(varList)
+                             
+                                                    
+                                                    break
+                                                        
+
+                                        
+                                        else:
+                                            pass
+                                    
+                                    else:
+                                        print('False')
+                                    
+
+                            else:
+                                p += 1
+                
+                else:
+                    o += 1
+
+    condition = False
+    breaker = False
+
+    if functionName[2] == '==':
+        main()
 
 
 # ============================================== Main ==============================================
@@ -386,6 +676,12 @@ while True:
     elif functionName[0] == 'varsList':
         varsList()
     
+    elif functionName[0] == 'group':
+        group()
+    
+    elif functionName[0] == 'if':
+        If()
+    
     elif functionName[0] == 'exit':
         break
 
@@ -404,3 +700,6 @@ while True:
 
     else:
         pass'''
+
+
+
